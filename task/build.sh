dc770b7f8dc6d82079eb77fdf1e5731ea1d79343
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-11-08 12:28:21 +0000

    Add todo for aws cli

--- task/build.sh
+++ task/build.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# TODO: replace with aws cli call to S3 download
 curl -o entity.sqlite https://files.temporary.digital-land.info/entity-builder/dataset/entity.sqlite3
 
 python3 build_tiles.py --entity-path entity.sqlite --output-dir /mnt/tiles
