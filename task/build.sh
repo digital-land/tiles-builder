d158ab4e87be18a3c1989274e39c4ccffeb3ed16
Lawrence Goldstien <lawrence.goldstien@madetech.com>
2022-11-16 12:01:27 +0000

    Tidy up tile builder script and add some debug messaging

--- task/build.sh
+++ task/build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-echo "$EVENT_ID: task running with env vars S3_BUCKET = $S3_BUCKET and S3_KEY = $S3_KEY"
-
 DATABASE=${S3_KEY##*/}
 DATABASE_NAME=${DATABASE%.*}
+
+echo "$EVENT_ID: running with settings: S3_BUCKET=$S3_BUCKET, S3_KEY=$S3_KEY, DATABASE=$DATABASE, DATABASE_NAME=$DATABASE_NAME"
 
 if [[ $DATABASE_NAME != "entity" ]]; then
   echo "$EVENT_ID: wrong database, skipping"
@@ -19,9 +19,9 @@ else
   date > /mnt/tiles/lock
 fi
 
-if ! [ -f $DATABASE_NAME.sqlite3 ]; then
+if ! [ -f "$DATABASE_NAME.sqlite3" ]; then
   echo "$EVENT_ID: attempting download from s3://$S3_BUCKET/$S3_KEY"
-  aws s3api get-object --bucket $S3_BUCKET --key $S3_KEYS $DATABASE_NAME.sqlite3 || \
+  aws s3api get-object --bucket "$S3_BUCKET" --key "$S3_KEY" "$DATABASE_NAME.sqlite3" || \
     echo "$EVENT_ID: failed to download from s3://$S3_BUCKET/$S3_KEY" && exit 1
 
   echo "$EVENT_ID: finished downloading from s3://$S3_BUCKET/$S3_KEY"
